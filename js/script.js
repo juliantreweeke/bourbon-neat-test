d173212cf534ae7107a63db3f837e14acea563ae
“juliantreweeke” <juliantreweeke@gmail.com>
2017-10-03 15:34:56 -0700

    working on nav draw

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-// alert("working");
+
+// document.getElementById("burger").onclick = function () {alert('onclick');};
+
+$("#burger").click(function() {
+  $(".nav a,.nav li").slideToggle("display");
+});
