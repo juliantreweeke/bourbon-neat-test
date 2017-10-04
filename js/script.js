1be858d68e64f760e25d44c4858e1cd091d593ee
“juliantreweeke” <juliantreweeke@gmail.com>
2017-10-04 05:04:00 -0700

    adding annotations, cleaning up code

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-
-// document.getElementById("burger").onclick = function () {alert('onclick');};
-
+// On burger click,open nav draw and fade out any other text.
 $("#burger").click(function() {
-  $("#toplinks").fadeToggle().addClass( "show" );
+  $("#toplinks").fadeToggle().addClass("show");
   $(".main-container").fadeToggle();
 });
 
-$( window ).resize(function() {
+// If window is more then 850 then put page out of mobile mode and back to default
+$(window).resize(function() {
   if ($(window).width() > 850) {
-    $( ".main-container" ).fadeIn();
-      $( "#toplinks" ).removeClass( "show").fadeIn();
-    }
-  
-  if ($(window).width() < 850){
+    $(".main-container").fadeIn();
+    $("#toplinks").removeClass("show").fadeIn();
+  }
+
+  // If window is less then 850 and toplinks does not have the show class, then hide it.
+  if ($(window).width() < 850) {
     if (!$("#toplinks").hasClass("show")) {
       $("#toplinks").css("display", "none");
     }
@@ -29,4 +29,3 @@ $(".circle").click(function() {
   $('.circle').not(this).removeClass('selected');
   $(this).toggleClass("selected");
 });
-
