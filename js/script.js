35a5afb53111c2dc08a5848b411a47cdf35a120f
“juliantreweeke” <juliantreweeke@gmail.com>
2017-10-03 17:55:19 -0700

    changed nav draw menu

--- js/script.js
+++ js/script.js
@@ -3,12 +3,14 @@
 
 $("#burger").click(function() {
   $( "#toplinks" ).toggleClass( "show");
+  $(".main-container").toggleClass("hide");
 });
 
 $( window ).resize(function() {
   if ($(window).width() > 850) {
   
       $( "#toplinks" ).removeClass( "show");
+      $( ".main-container" ).removeClass( "hide");
     } 
 });
 
