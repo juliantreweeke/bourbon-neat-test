f038f11976879d9e0ae5e1fbd707a27ceaac6b2f
“juliantreweeke” <juliantreweeke@gmail.com>
2017-10-03 20:02:25 -0700

    giving up on svg viewbox for now added svg responsive css

--- js/script.js
+++ js/script.js
@@ -1,33 +1,30 @@
 
 // document.getElementById("burger").onclick = function () {alert('onclick');};
 
-
-
-
-
 $("#burger").click(function() {
-  
-  // $(".main-container").toggleClass("hide");
-  $("#toplinks").fadeToggle();
-  // $( "#toplinks" ).toggleClass( "show");
+  $("#toplinks").fadeToggle().addClass( "show" );
   $(".main-container").fadeToggle();
 });
+
 
 
 $( window ).resize(function() {
   if ($(window).width() > 850) {
       $( "#toplinks" ).removeClass( "show").fadeIn();
-      $( ".main-container" ).removeClass( "hide").fadeIn();
-    } 
+      $( ".main-container" ).fadeIn();
+    }
   
-  if ($(window).width() < 850) {
-      $( "#toplinks" ).addClass( "show" ).hide();
-    } 
-  // if ($window.width() === 850 ){
-  //   $( "#toplinks" ).hide();
-  // }
+  // if ($(window).width() === 850){
+  //   $("#toplinks").fadeOut(-10);
+  //   }
+  
+
+    
+    // if ($(window).width() === 850) {
+    //     $( "#toplinks" ).fadeOut(-10);
+    //   } 
 });
 
-
-
-
+$(".circle").click(function() {
+    $(this).toggleClass("selected");
+});
