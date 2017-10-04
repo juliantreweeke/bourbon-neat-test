440c206dc72a08518d622d3726906739003ed392
“juliantreweeke” <juliantreweeke@gmail.com>
2017-10-03 18:22:01 -0700

    trying to fix fading bugs

--- js/script.js
+++ js/script.js
@@ -1,21 +1,33 @@
 
 // document.getElementById("burger").onclick = function () {alert('onclick');};
 
+
+
+
+
 $("#burger").click(function() {
   
   // $(".main-container").toggleClass("hide");
-  $("#toplinks").fadeToggle().toggleClass( "show");
+  $("#toplinks").fadeToggle();
   // $( "#toplinks" ).toggleClass( "show");
   $(".main-container").fadeToggle();
 });
 
+
 $( window ).resize(function() {
   if ($(window).width() > 850) {
-  
-      $( "#toplinks" ).removeClass( "show");
-      $( ".main-container" ).removeClass( "hide");
+      $( "#toplinks" ).removeClass( "show").fadeIn();
+      $( ".main-container" ).removeClass( "hide").fadeIn();
     } 
+  
+  if ($(window).width() < 850) {
+      $( "#toplinks" ).addClass( "show" ).hide();
+    } 
+  // if ($window.width() === 850 ){
+  //   $( "#toplinks" ).hide();
+  // }
 });
+
 
 
 
