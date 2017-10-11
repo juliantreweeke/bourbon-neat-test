b249dfa630ab8e16201c9d0152dfa94bc93c224c
“juliantreweeke” <juliantreweeke@gmail.com>
2017-10-11 10:50:51 -0700

    starting the mobile round 2

--- js/script.js
+++ js/script.js
@@ -1,22 +1,43 @@
 // On burger click,open nav draw and fade out any other text.
 $("#burger").click(function() {
-  $("#toplinks").fadeToggle().addClass("show");
-  $(".main-container").fadeToggle();
+  $("#toplinks").addClass("show");
+  // $("#toplinks").toggleClass("hidden");
+  if( $("#toplinks").css('display').toLowerCase() == 'none') {
+
+  $("#toplinks").css("display", "block");
+  // $(".show").css("display", "block");
+
+  } else {
+  $("#toplinks").css("display", "none");
+  }
+
+  // $("#toplinks").css("display", "block");
+
+  // $("#toplinks").fadeToggle().addClass("show").toggleClass("hidden");
+  // $( "#toplinks" ).fadeToggle().slideDown( "slow", function() {
+  //   $(this).addClass("show");
+  // });
+
+  // $(".main-container").fadeToggle();
+
+  // $("nav").addClass("nav-background");
+
+
 });
 
 // If window is more then 850 then put page out of mobile mode and back to default
 $(window).resize(function() {
   if ($(window).width() > 850) {
-    $('body').css('overflowY', 'hidden');
     $(".main-container").fadeIn();
     $("#toplinks").removeClass("show").fadeIn();
   }
 
   // If window is less then 850 and toplinks does not have the show class, then hide it.
   if ($(window).width() < 850) {
-    $('body').css('overflowY', 'auto');
     if (!$("#toplinks").hasClass("show")) {
+
       $("#toplinks").css("display", "none");
+      // $("#toplinks").toggleClass("hidden");
     }
   }
 
