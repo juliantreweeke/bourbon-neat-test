c3acf81cf5713440c6fe4e63d7a39345e556f980
“juliantreweeke” <juliantreweeke@gmail.com>
2017-10-13 04:29:25 -0700

    animated burger

--- js/script.js
+++ js/script.js
@@ -1,42 +1,46 @@
-// On burger click, slide open nav draw.
 
-$("#burger").click(function() {
+$(document).ready(function(){
+  // On burger click, slide open nav draw.
+  $("#burger").click(function() {
 
-  $("#toplinks").addClass("show").css("display", "block");
+    $(this).toggleClass("open");
 
-  if (!$(".show").hasClass("active")) {
-    $(".show").animate({"margin-right": '0%'}).addClass("active");
-    $(this).find($(".fa")).removeClass('fa-bars').addClass('fa-close-thin');
-  } else {
-    $(".show").animate({"margin-right": '+=100%'}).removeClass("active");
-    $(this).find($(".fa")).removeClass('fa-close-thin').addClass('fa-bars');
-  }
+    // change menu links into mobile mode
+    $("#toplinks").addClass("show").css("display", "block");
 
-});
-
-// If window is more then 850 then put page out of mobile/tablet mode and back to default
-$(window).resize(function() {
-  if ($(window).width() > 850) {
-    $(".show").removeClass("active").css({"margin-right":'0%'});
-    $("#toplinks").removeClass("show").fadeIn();
-    $("#burger").find($(".fa")).removeClass('fa-close-thin').addClass('fa-bars');
-  }
-
-  // If window is less then 850 and toplinks does not have the show class, then hide it.
-  if ($(window).width() < 850) {
-    if (!$("#toplinks").hasClass("show")) {
-      $("#toplinks").css("display", "none");
+    // nav slider
+    if (!$(".show").hasClass("active")) {
+      $(".show").animate({"margin-right": '0%'}).addClass("active");
+    } else {
+      $(".show").animate({"margin-right": '+=100%'}).removeClass("active");
     }
-  }
+  });
 
-});
+  // If window is more then 850 then put page out of mobile/tablet mode and back to default
+  $(window).resize(function() {
+    if ($(window).width() > 850) {
+      $(".show").removeClass("active").css({"margin-right":'0%'});
+      $("#toplinks").removeClass("show").fadeIn();
+      $('#burger').removeClass("open");
+    }
 
-// Toggle css between the center dot links
+    // If window is less then 850 and toplinks does not have the show class, then hide it.
+    if ($(window).width() < 850) {
+      if (!$("#toplinks").hasClass("show")) {
+        $("#toplinks").css("display", "none");
+      }
+    }
 
-$(".circle").click(function() {
-  if ($(this).hasClass("selected")) {
-    return;
-  }
-  $('.circle').not(this).removeClass('selected');
-  $(this).toggleClass("selected");
+  });
+
+  // Toggle css between the center dot links
+
+  $(".circle").click(function() {
+    if ($(this).hasClass("selected")) {
+      return;
+    }
+    $('.circle').not(this).removeClass('selected');
+    $(this).toggleClass("selected");
+  });
+
 });
